8c91b76a83caca7801d502c622b57860a9aef43f
OyeniyiRejoice <oyeniyi229@gmail.com>
2025-10-28 20:57:06 +0000

    Fix: Corrected missing app.css import path for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,4 @@
-/*
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import LandingPage from "./pages/LandingPage";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import TicketManager from "./pages/TicketManager";
-import "./app.css";
 
-function App() {
-  return (
-    
-      <Layout>
-      
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/tickets" element={<TicketManager />} />
-      </Routes>
-      </Layout>
-  
-  );
-}
-
-export default App;*/
 
 
 import { Routes, Route } from "react-router-dom";
@@ -36,7 +9,7 @@ import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import TicketManager from "./pages/TicketManager";
 import PrivateRoute from "./utils/PrivateRoute";
-import "./app.css";
+import "./App.css";
 
 function App() {
   return (
